a2f41bbc085dfd33c6c6b730b5bee622b7c19f13
nikhilkumar kekan <nikhil19kekan@gmail.com>
2018-02-15 19:45:52 -0600

    datastructure practice answers

---
+++ DataStructures/p2/ans.c
@@ -0,0 +1,35 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int arr[6][6];
+    for(int arr_i = 0; arr_i < 6; arr_i++){
+       for(int arr_j = 0; arr_j < 6; arr_j++){
+          
+          scanf("%d",&arr[arr_i][arr_j]);
+       }
+    }
+    static int final;
+    int sum;
+    for(int i=0;i<=(6/2);i++){
+        for(int j=0;j<=(6/2);j++){
+            sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            if(i==0 && j==0){
+                final = sum;
+            }else{
+                if(sum > final){
+                    //printf("final:%d\n",final);
+                    final = sum;
+                }
+            }
+        }
+    }
+    printf("%d",final);
+    return 0;
+}
+
